d7a377a1ac23fe77a2b4ddc7fcaedc205c801e2e
niawjunior <pasupol@dome.cloud>
2019-09-02 14:16:37 +0700

    add test case | should return json body in response

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -9,18 +9,24 @@ let res = httpMock.createResponse()
 let next = null
 
 describe('TodoController', () => {
+  beforeEach(() => {
+    req.body = newTodo
+  })
   it('should have a createTodo function', () => {
     expect(typeof TodoController.createTodo).toBe('function')
   })
   it('should call TodoModel.create', () => {
-    req.body = newTodo
     TodoController.createTodo(req, res, next)
     expect(TodoModel.create).toBeCalledWith(req.body)
   })
   it('should return 201 response code', () => {
-    req.body = newTodo
     TodoController.createTodo(req, res, next)
     expect(res.statusCode).toBe(201)
     expect(res._isEndCalled).toBeTruthy()
+  })
+  it('should return json body in response', () => {
+    TodoModel.create.mockReturnValue(newTodo)
+    TodoController.createTodo(req, res, next)
+    expect(res._getJSONData()).toStrictEqual(newTodo)
   })
 })
