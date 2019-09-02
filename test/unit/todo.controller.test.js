cbdf31bf2d2582a83fb1939b409064c1dc095447
niawjunior <pasupol@dome.cloud>
2019-09-02 14:01:36 +0700

    add test case | should return 201 response code

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -4,17 +4,23 @@ const httpMock = require('node-mocks-http')
 const newTodo = require('../mock/new-todo.json')
 TodoModel.create = jest.fn()
 
+let req = httpMock.createRequest()
+let res = httpMock.createResponse()
+let next = null
 
 describe('TodoController', () => {
   it('should have a createTodo function', () => {
     expect(typeof TodoController.createTodo).toBe('function')
   })
   it('should call TodoModel.create', () => {
-    let req = httpMock.createRequest()
-    let res = httpMock.createResponse()
-    let next = null
     req.body = newTodo
     TodoController.createTodo(req, res, next)
     expect(TodoModel.create).toBeCalledWith(req.body)
+  })
+  it('should return 201 response code', () => {
+    req.body = newTodo
+    TodoController.createTodo(req, res, next)
+    expect(res.statusCode).toBe(201)
+    expect(res._isEndCalled).toBeTruthy()
   })
 })
